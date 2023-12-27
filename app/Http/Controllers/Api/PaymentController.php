ebdca3cabcb0c2b21aca7e337a857a1fc2d1d541
unknown <developer@codesystem.co.in>
2023-12-27 21:37:18 +0530

    add advertisement

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -59,8 +59,11 @@ class PaymentController extends BaseController
 		
 		//$api_key='rzp_test_fbiwRvGp1057Ua';
 		//$api_secret='O4q2Dcq4PhZHNAwYvXifusHF';
-		$api_key='rzp_test_bmd8yXq2RAx4Uy';
-		$api_secret='rQ9oPkuZ0CdQcL2A9TbPVf3g';
+		//$api_key='rzp_test_bmd8yXq2RAx4Uy';
+	//	$api_secret='rQ9oPkuZ0CdQcL2A9TbPVf3g';
+		
+		$api_key='rzp_live_bhuyXPXQEwDPjI';
+		$api_secret='Jw41w0fZnCUcobdfTp4w7oYb';
 		
 		$validator = Validator::make($postData, [
                 'amount'=>'required',
