bcacfc522fadf87214e5806a105880fd7356ae6a
unknown <developer@codesystem.co.in>
2024-02-16 12:42:28 +0530

    changes

--- app/Models/PoultryHatchery.php
+++ app/Models/PoultryHatchery.php
@@ -12,7 +12,7 @@ class PoultryHatchery extends Model
 {
     use HasFactory,LogsActivity;
 	protected $table = 'poultryhatchery_centers';
-    protected $fillable = ['id','email_id','user_id','breed','price_chick','user_code','latitude','longitude','subscriptionStartDate','subscriptionEndDate','taluka','district',
+    protected $fillable = ['id','registration_number','email_id','user_id','breed','price_chick','user_code','latitude','longitude','subscriptionStartDate','subscriptionEndDate','taluka','district',
 	'poultryhatchery_center_name','type','incharge_name','description','mobile_number','contact_number','address','city_id','city_town','state_id','state','pincode','created_at','updated_at','deleted_at','status'];
         
     public static function boot()
